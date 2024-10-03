580bd9d3cd4e5275034ab17dd73e6018dbdea68a
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-10-03 22:43:21 +0300

    fix(style added DataSet.ts)

---
+++ front/src/data/DataSet.ts
@@ -0,0 +1,12 @@
+export type UsersType = {
+    id: number,
+    name: string
+}
+
+export const usersData: UsersType[] = [
+  {id: 1, name: 'Серёгин И.В.'},
+  {id: 2, name: 'Мясников С.А.'},
+  {id: 3, name: 'Кирюшин И.В.'},
+  {id: 4, name: 'Фёдоров С.Н.'},
+  {id: 5, name: 'Гончаров И.Ю.'},
+];
