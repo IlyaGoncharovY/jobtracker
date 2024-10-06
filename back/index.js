9515ff3c47ac58f96a9006b1de5f3b558ba894d8
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-10-06 19:31:51 +0300

    feat(added express)

--- back/index.js
+++ back/index.js
@@ -1,10 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const webTMAUrl = 'https://ilyagoncharovy.github.io/jobtracker/';
 
 const bot = new TelegramBot(token, {polling: true});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
@@ -55,3 +62,8 @@ bot.on('message', async (msg) => {
         }
     }
 });
+
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log(`server started on PORT: ${PORT}`));
