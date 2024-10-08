1642af956c6c495f17bad524f9c7215a45d472dd
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-10-08 18:55:37 +0300

    fix(node) refactor .env

--- back/index.js
+++ back/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const webhookUrl = 'https://jobtracker-l44k.onrender.com';
+const webhookUrl = process.env.URL_SERVER;
 
 const bot = new TelegramBot(token);
 bot.setWebHook(webhookUrl);
