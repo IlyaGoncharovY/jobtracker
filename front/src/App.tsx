6c83b5399cc6f7bb098d4ec3fa514840d909c124
Илья <93834466+IlyaGoncharovY@users.noreply.github.com>
2024-11-17 01:48:37 +0300

    Merge pull request #20
    * fix lint err
    
    * feat (added lib) added framer-motion
    
    * fix(App) added AnimatePresence wrapper for render animation on scroll
    
    * feat(form wrapper) added FormWrapper.tsx - component wrapper for form…
    
    * fix(forms) use FormWrapper im forms.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,8 @@
 import {useEffect} from 'react';
 
-import {Navigate, Route, Routes} from 'react-router-dom';
+import {Navigate, Route, Routes, useLocation} from 'react-router-dom';
+
+import {AnimatePresence} from 'framer-motion';
 
 import s from './App.module.css';
 import {useTelegram} from './common/customHook';
@@ -9,6 +11,7 @@ import {CommissionMode, Header, RadioStationMode} from './components';
 export const App = () => {
 
   const {tg} = useTelegram();
+  const location = useLocation();
 
   useEffect(() => {
     tg.ready();
@@ -17,11 +20,13 @@ export const App = () => {
   return (
     <div className={s.AppContainer}>
       <Header/>
-      <Routes>
-        <Route path="/" element={<CommissionMode />} />
-        <Route path="/RadioStationMode" element={<RadioStationMode />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<CommissionMode />} />
+          <Route path="/RadioStationMode" element={<RadioStationMode />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 };
--- front/src/common/components/index.ts
+++ front/src/common/components/index.ts
@@ -1,3 +1,4 @@
 export * from './select/SelectContainer.tsx';
 export * from './input/dateInput/DateInput.tsx';
 export * from './formContainer/FormContainer.tsx';
+export * from './wrapper/FormWrapper.tsx';
