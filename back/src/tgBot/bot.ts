17acc7bf08e3cdcf4343e3be75e7c74de6ae86e1
Илья <93834466+IlyaGoncharovY@users.noreply.github.com>
2025-02-05 01:03:51 +0300

    dev (#37)
    * fix (structure) refactor structure in all project
    
    * fix (bot.ts) delete polling (#34)
    
    * fix (polling)

--- back/src/tgBot/bot.ts
+++ back/src/tgBot/bot.ts
@@ -13,7 +13,8 @@ const webGoogleSheetUrl = 'https://docs.google.com/spreadsheets/d/1zsAZjXsQPDBvx
 const webYandexTechCardUrl = 'https://disk.yandex.ru/d/iA59ojO89g5vDw';
 const dwarfFightGameUrl = 'https://ilyagoncharovy.github.io/dwarf-fight/';
 
-const bot = new TelegramBot(token);
+
+const bot = new TelegramBot(token, { polling: true });
 bot.setWebHook(webhookUrl);
 
 const participants = new Set<number>();
